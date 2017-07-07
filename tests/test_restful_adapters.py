2fc9a1b8dbb52b00d1c1557c10d343b04399bcee
Colin Hostetter <colin.hostetter@plangrid.com>
2017-07-06 17:02:32 -0700

    Fix tests to work in CI

--- tests/test_restful_adapters.py
+++ tests/test_restful_adapters.py
@@ -1,6 +1,4 @@
-import json
-from unittest import TestCase
-
+from flask_testing import TestCase
 from flask import Blueprint
 from flask import Flask
 
@@ -9,54 +7,51 @@ from plangrid.flask_toolbox.restful_adapters import RestfulApiAdapter
 from plangrid.flask_toolbox.http_errors import BadRequest
 
 class TestRestfulApiAdapter(TestCase):
-    def make_app(self, handler, route, methods):
+    def create_app(self):
         app = Flask(__name__)
         Toolbox(app)
         blueprint = Blueprint('test', __name__)
         adapter = RestfulApiAdapter(blueprint)
-        adapter.add_resource(handler, route, methods)
+        adapter.add_resource(SimpleHandler, '/thing', ['GET', 'POST'])
+        adapter.add_resource(DictRespondingHandler, '/dict', ['GET'])
+        adapter.add_resource(EmptyResponseHandler, '/nothing', ['GET'])
+        adapter.add_resource(KwargsHandler, '/exclaim/<string:word>', ['GET'])
+        adapter.add_resource(ErrorThrowingHandler, '/error', ['GET'])
         app.register_blueprint(blueprint)
         return app
 
     def test_http_methods(self):
-        client = self.make_app(SimpleHandler, '/thing', ['GET', 'POST']).test_client()
-
-        resp = client.get('/thing')
+        resp = self.app.test_client().get('/thing')
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.data, 'hello')
+        self.assertEqual(resp.data.decode('utf-8'), 'hello')
 
-        resp = client.post('/thing')
+        resp = self.app.test_client().post('/thing')
         self.assertEqual(resp.status_code, 201)
-        self.assertEqual(resp.data, 'goodbye')
+        self.assertEqual(resp.data.decode('utf-8'), 'goodbye')
 
-        resp = client.delete('/thing')
+        resp = self.app.test_client().delete('/thing')
         self.assertEqual(resp.status_code, 405)
 
-        resp = client.get('/stuff')
+        resp = self.app.test_client().get('/stuff')
         self.assertEqual(resp.status_code, 404)
 
     def test_dict_response(self):
-        client = self.make_app(DictRespondingHandler, '/dict', ['GET']).test_client()
-        resp = client.get('/dict')
+        resp = self.app.test_client().get('/dict')
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(json.loads(resp.data), TEST_DICT)
+        self.assertEqual(resp.json, TEST_DICT)
 
     def test_empty_response(self):
-        client = self.make_app(EmptyResponseHandler, '/nothing', ['GET']).test_client()
-        resp = client.get('/nothing')
-        self.assertEqual(resp.data, '')
+        resp = self.app.test_client().get('/nothing')
+        self.assertEqual(resp.data.decode('utf-8'), '')
 
     def test_kwargs(self):
-        client = self.make_app(KwargsHandler, '/exclaim/<string:word>', ['GET']).test_client()
-        resp = client.get('/exclaim/hello')
-        self.assertEqual(resp.data, 'hello!')
+        resp = self.app.test_client().get('/exclaim/hello')
+        self.assertEqual(resp.data.decode('utf-8'), 'hello!')
 
     def test_error_handler(self):
-        app = self.make_app(ErrorThrowingHandler, '/error', ['GET'])
-        client = app.test_client()
-        resp = client.get('/error')
+        resp = self.app.test_client().get('/error')
         self.assertEqual(resp.status_code, 400)
-        self.assertEqual(json.loads(resp.data)['message'], BadRequest.default_message)
+        self.assertEqual(resp.json, {'message': BadRequest.default_message})
 
 TEST_DICT = {'some': 'stuff', 'in_a': 'dict'}
 
