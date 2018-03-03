1509fd829a0ccb3a3f7425501166c13fc3af06b0
barakalon <barak@plangrid.com>
2018-03-02 18:20:22 -0800

    remove client_test since its not working for python2.7 and needs more testing/documentation

--- setup.py
+++ setup.py
@@ -4,18 +4,37 @@ from setuptools import setup
 
 if __name__ == '__main__':
     setup(
+        name='flask-rebar',
+        version='0.1.0',
         author='Barak Alon',
         author_email='barak.s.alon@gmail.com',
         description='Flask-Rebar combines flask, marshmallow, and swagger for robust REST services.',
         long_description=open('README.rst').read(),
         keywords=['flask', 'rest', 'marshmallow', 'openapi', 'swagger'],
-        name='flask-rebar',
+        license='MIT',
         packages=['flask_rebar'],
         install_requires=[
             'Flask>=0.10',
             'marshmallow>=2.13',
         ],
-        version='0.1.0',
         zip_safe=True,
-        url='https://github.com/plangrid/flask-rebar'
+        url='https://github.com/plangrid/flask-rebar',
+        classifiers=[
+            'Environment :: Web Environment',
+            'Framework :: Flask',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+            'Topic :: Software Development :: Libraries :: Application Frameworks',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
     )
