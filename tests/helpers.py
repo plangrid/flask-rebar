8c5365336f55b8678b7affbe816a9c9c76c773bb
Andrew Standley <andrew.standley@plangrid.com>
2019-09-16 12:41:45 -0700

    Fix marshmallow test helpers so that they work will all unittest compatible frameworks and not just pytest. 'python setup.py test'  works again.

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,11 +1,10 @@
-import pytest
-
+import unittest
 from flask_rebar.compat import MARSHMALLOW_V2, MARSHMALLOW_V3
 
 
-skip_if_marshmallow_not_v2 = pytest.mark.skipif(
+skip_if_marshmallow_not_v2 = unittest.skipIf(
     not MARSHMALLOW_V2, reason="Only applicable for Marshmallow version 2"
 )
-skip_if_marshmallow_not_v3 = pytest.mark.skipif(
+skip_if_marshmallow_not_v3 = unittest.skipIf(
     not MARSHMALLOW_V3, reason="Only applicable for Marshmallow version 3"
 )
