f733e3e87eb8961d039fadc962de5248a930868b
Ali Scott <aliscott913@gmail.com>
2020-08-12 16:47:52 -0400

    adding test coverage (#201)
    * adding test coverage
    https://jira.autodesk.com/browse/ACSBP-1520
    https://github.com/plangrid/flask-rebar/issues/141
    
    * test coverage fixes
    https://jira.autodesk.com/browse/ACSBP-1520
    https://github.com/plangrid/flask-rebar/issues/141
    
    * Test coverage
    https://jira.autodesk.com/browse/ACSBP-1520
    https://github.com/plangrid/flask-rebar/issues/141
    
    * clean up
    https://jira.autodesk.com/browse/ACSBP-1520
    https://github.com/plangrid/flask-rebar/issues/141
    
    * clean up
    https://jira.autodesk.com/browse/ACSBP-1520
    https://github.com/plangrid/flask-rebar/issues/141
    
    * clean up from pr comments
    https://jira.autodesk.com/browse/ACSBP-1520
    https://github.com/plangrid/flask-rebar/issues/141

---
+++ tests/swagger_generation/test_swagger_generator_hidden_api.py
@@ -0,0 +1,74 @@
+"""
+    Test Swagger Generation
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    Tests for converting a handler registry to a Swagger specification.
+
+    :copyright: Copyright 2018 PlanGrid, Inc., see AUTHORS.
+    :license: MIT, see LICENSE for details.
+"""
+import json
+
+import marshmallow as m
+import pytest
+
+from flask_rebar.rebar import Rebar
+from flask_rebar.swagger_generation import ExternalDocumentation
+from flask_rebar.swagger_generation import SwaggerV2Generator
+from flask_rebar.swagger_generation import SwaggerV3Generator
+from flask_rebar.swagger_generation import Server
+from flask_rebar.swagger_generation import ServerVariable
+from flask_rebar.swagger_generation import Tag
+from flask_rebar.testing import validate_swagger
+from flask_rebar.testing.swagger_jsonschema import (
+    SWAGGER_V2_JSONSCHEMA,
+    SWAGGER_V3_JSONSCHEMA,
+)
+
+from tests.swagger_generation.registries import hidden_api
+
+
+def _assert_dicts_equal(a, b):
+    result = json.dumps(a, indent=2, sort_keys=True)
+    expected = json.dumps(b, indent=2, sort_keys=True)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "registry, swagger_generator, expected_swagger",
+    [
+        (
+            hidden_api.registry,
+            hidden_api.swagger_v2_generator,
+            hidden_api.EXPECTED_SWAGGER_V2,
+        ),
+        (
+            hidden_api.registry,
+            hidden_api.hidden_swagger_v3_generator,
+            hidden_api.EXPECTED_HIDDEN_SWAGGER_V3,
+        ),
+        (
+            hidden_api.registry,
+            hidden_api.swagger_v3_generator,
+            hidden_api.EXPECTED_SWAGGER_V3,
+        ),
+    ],
+)
+def test_swagger_generators(registry, swagger_generator, expected_swagger):
+    open_api_version = swagger_generator.get_open_api_version()
+    if open_api_version == "2.0":
+        swagger_jsonschema = SWAGGER_V2_JSONSCHEMA
+    elif open_api_version == "3.0.2":
+        swagger_jsonschema = SWAGGER_V3_JSONSCHEMA
+    else:
+        raise ValueError("Unknown swagger_version: {}".format(open_api_version))
+
+    validate_swagger(expected_swagger, schema=swagger_jsonschema)
+
+    swagger = swagger_generator.generate(registry)
+
+    result = json.dumps(swagger, indent=2, sort_keys=True)
+    expected = json.dumps(expected_swagger, indent=2, sort_keys=True)
+
+    assert result == expected
