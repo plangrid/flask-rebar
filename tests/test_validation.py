a27e705601ef2a7e320dc103e669869e28445c4f
Ali Scott <aliscott913@gmail.com>
2020-08-21 16:45:34 -0400

    Dropping Marshallow_v2 support (#195)
    * Dropping Marshallow_v2 support
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * Dropping Marshallow_v2 support
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * Dropping Marshallow_v2 support
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * bumping up to marshmallowV3 in pull requests
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * bumping up to marshmallowV3 in pull requests
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * Fixing function signatures
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * Bug fixes
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * Bug fixes
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * testing bug failures
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * test failure messages fix
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * updated bug fixes
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * bug fixes
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * bug fixes
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * fix: nested field is a collection
    
    * fix: black
    
    * dropping python 2.7
    https://jira.autodesk.com/browse/ACSBP-1523
    https://github.com/plangrid/flask-rebar/issues/142
    
    * dropping python 2.7
    https://jira.autodesk.com/browse/ACSBP-1523
    https://github.com/plangrid/flask-rebar/issues/142
    
    * pr comment changes
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * pr comment changes
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * pr comment changes
    https://jira.autodesk.com/browse/ACSBP-1518
    https://github.com/plangrid/flask-rebar/issues/37
    
    * pr comment changes
    
    * Streamline compat (#210)
    
    * Dropped this from last commit
    
    Co-authored-by: Rick Riensche <rick.riensche@autodesk.com>
    Co-authored-by: Matthew Janes <mjanes@gmail.com>

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -17,9 +17,7 @@ from marshmallow.validate import OneOf
 from werkzeug.datastructures import MultiDict
 
 from flask_rebar import compat
-from flask_rebar import messages
 from flask_rebar.utils.request_utils import normalize_schema
-from tests.helpers import skip_if_marshmallow_not_v2
 from flask_rebar.validation import RequireOnDumpMixin
 from flask_rebar.validation import CommaSeparatedList
 from flask_rebar.validation import DisallowExtraFieldsMixin
@@ -29,35 +27,6 @@ from flask_rebar.validation import QueryParamList
 class DisallowExtraFieldsSchema(Schema, DisallowExtraFieldsMixin):
     a = fields.String()
     b = fields.String(load_from="c")
-
-
-@skip_if_marshmallow_not_v2
-class TestDisallowExtraFieldsMixin(TestCase):
-    def test_nominal(self):
-        _, errors = DisallowExtraFieldsSchema(strict=True).dump({})
-        self.assertEqual(errors, {})
-
-    def test_unexpected_field(self):
-        data, errs = DisallowExtraFieldsSchema().load({"foo": "bar"})
-        self.assertEqual(errs, {"_schema": [messages.unsupported_fields(["foo"])]})
-
-    def test_respects_load_from_and_attribute(self):
-        data, errors = DisallowExtraFieldsSchema().load({"c": "bar"})
-        self.assertEqual({}, errors)
-        self.assertEqual(data, {"b": "bar"})
-
-    def test_respects_exclude(self):
-        schema = DisallowExtraFieldsSchema(exclude=("a",))
-        data, errors = schema.load({"a": "yz"})
-        self.assertEqual(errors, {})
-        self.assertEqual(data, {})
-
-    def test_doesnt_break_for_non_object_schema(self):
-        data, errors = DisallowExtraFieldsSchema().load(
-            ["im not supposed to be a list :)"]
-        )
-        self.assertEqual(errors, {"_schema": ["Invalid input type."]})
-        self.assertFalse(data)
 
 
 class RequireOnDumpMixinSchema(Schema, RequireOnDumpMixin):
@@ -106,8 +75,7 @@ class RequireOutputMixinTest(TestCase):
             compat.dump(self.schema, self.data)
         # it's some sort of date error
         self.assertIn(
-            "cannot be formatted as a datetime",
-            ctx.exception.messages["validation_required"][0],
+            "Not a valid datetime", ctx.exception.messages["validation_required"][0]
         )
 
     def test_required_failed_validate(self):
@@ -156,14 +124,13 @@ class TestCommaSeparatedList(TestCase):
 
     def test_serialize_errors(self):
         with self.assertRaises(ValidationError) as ctx:
-            compat.dump(IntegerList(), {"foos": [1, "two"]})
+            compat.dump(IntegerList(), {"foos": [42, "two"]})
 
         self.assertEqual(
             ctx.exception.messages,
             {
-                # Marshmallow's fields.List formats the dump errors differently
-                # than load :shrug:
-                "foos": ["Not a valid integer."]
+                # Element 1 in our list should produce error:
+                "foos": {1: ["Not a valid integer."]}
             },
         )
 
