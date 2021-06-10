823b4c2a913fe9fa45055a07508ede80a2a84851
Eugene Kim <eugene.kim@autodesk.com>
2021-06-10 15:07:33 +0100

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ development = [
 if __name__ == "__main__":
     setup(
         name="flask-rebar",
-        version="2.0.0rc2",
+        version="2.0.0rc3",
         author="Barak Alon",
         author_email="barak.s.alon@gmail.com",
         description="Flask-Rebar combines flask, marshmallow, and swagger for robust REST services.",
