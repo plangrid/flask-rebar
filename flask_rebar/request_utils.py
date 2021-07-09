ed2b19fa594ee945731a45f26817b7f343e39896
Rick Riensche <rick.riensche@autodesk.com>
2021-07-09 12:07:09 -0700

    Revert second-guessing of deprecation
    Pending ongoing discussion around balance between hand-holding and tech debt ;) 
    Assuming we do deprecate stuff will create a dedicated PR for that.

--- flask_rebar/request_utils.py
+++ flask_rebar/request_utils.py
@@ -7,7 +7,4 @@ from flask_rebar.request_utils import marshal
 
 When we release 2.0 and can make breaking changes, this stub should be removed as we are
 consolidating "utility" modules such as request_utils.py into flask_rebar.utils
-
-July2021 On further reflection as we near release of 2.0 - eeeeh we don't NEED to delete this, let's avoid
-breaking change for sake of breaking change stuff ;)
 """
