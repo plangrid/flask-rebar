8a8173663d5b00760de52d9fd1cc16ae978908a2
Daniel Wallace <danielwallace@gtmanfred.com>
2023-10-26 13:55:06 -0400

    Bump version: 3.0.0 → 3.1.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ development = [
 if __name__ == "__main__":
     setup(
         name="flask-rebar",
-        version="3.0.0",
+        version="3.1.0",
         author="Barak Alon",
         author_email="barak.s.alon@gmail.com",
         description="Flask-Rebar combines flask, marshmallow, and swagger for robust REST services.",
